3ce3bd950e6af2564f46ccfb3f8f8a31aba9bee5
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-07 16:09:37 +0530

    Update App.java

--- SpringBootAutowire/App.java
+++ SpringBootAutowire/App.java
@@ -1,1 +1,18 @@
+package com.ebaad.SpringBootAutowireDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = SpringApplication.run(App.class, args);
+        Cars cars = context.getBean(Cars.class);
+        
+        cars.sale();
+    }
+}
 
