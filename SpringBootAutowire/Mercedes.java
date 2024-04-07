00c570726a87f6359f19d4e3d596d1b489e0ad9c
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-07 16:11:57 +0530

    Update Mercedes.java

--- SpringBootAutowire/Mercedes.java
+++ SpringBootAutowire/Mercedes.java
@@ -1,1 +1,27 @@
+package com.ebaad.SpringBootAutowireDemo;
+
+import org.springframework.stereotype.Component;
+
+/*
+ * When we use the annotation @Component then Spring Framework is responsible for creating the object of
+ * Cars using getBeans(). @Component is a Spring annotation used to indicate that a Java class is a Spring
+ * component. Spring components are Java classes managed by the Spring IoC (Inversion of Control) container.
+ * */
+@Component
+public class Mercedes {
+	
+	private int currentSpeed;
+	private int maxSpeed;
+	private int noOfGears;
+	
+	public Mercedes() {
+		currentSpeed = 0;
+		maxSpeed = 400;
+		noOfGears = 5;
+	}
+	
+	public void speedAndGears() {
+		System.out.println("The Mercedes has "+ noOfGears + " gears in total, Maximum Speed as " + maxSpeed + " and its current speed is " + currentSpeed);
+	}
+}
 
