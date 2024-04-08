035aba00debb64cfa2c702463fff3d3ce7587a1a
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-08 16:27:40 +0530

    Update Cars.java

--- Dependency Injection/Spring Singleton and Prototype Scopes/Cars.java
+++ Dependency Injection/Spring Singleton and Prototype Scopes/Cars.java
@@ -1,1 +1,33 @@
+package com.ebaad.SpringSingletonOrPrototypeScope;
+
+public class Cars {
+	private String brand;
+	private String model;
+	private int year;
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cars [brand=" + brand + ", model=" + model + ", year=" + year + "]";
+	}
+
+}
 
