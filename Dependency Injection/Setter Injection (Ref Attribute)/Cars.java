438fb9deaabb2d4f434c681b417edd72aa06bbc6
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-09 16:57:10 +0530

    Update Cars.java

--- Dependency Injection/Setter Injection (Ref Attribute)/Cars.java
+++ Dependency Injection/Setter Injection (Ref Attribute)/Cars.java
@@ -1,1 +1,21 @@
+package com.ebaad.SpringSetterInjection;
+
+public class Cars {
+	// The variable of type SportsCar
+	private SportsCar sports;
+	
+	// Method
+	public void drive() {
+		System.out.println("A normal car can drive smoothly");
+		sports.speed(); // calling the method of SportsCar
+	}
+	
+	// Setters and Getters
+	public SportsCar getSports() {
+		return sports;
+	}
+	public void setSports(SportsCar sports) {
+		this.sports = sports;
+	}
+}
 
