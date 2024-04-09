edfc4e9c35077c7bda728efd0a15e355d4d4d0e7
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-09 16:32:36 +0530

    Update Cars.java

--- Dependency Injection/Setter Injection (Value Attribute)/Cars.java
+++ Dependency Injection/Setter Injection (Value Attribute)/Cars.java
@@ -1,1 +1,14 @@
+package com.ebaad.SpringSetterInjection;
+public class Cars {
+	// The default value of milleage is set in the xml file using property tag
+	private int milleage;
+	
+	public void setMilleage(int milleage) {
+		this.milleage=milleage;
+	}
+	
+	public int getMilleage() {
+		return milleage;
+	}
+}
 
