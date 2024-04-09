3363eb986b54dec8086711d6552592508f604d03
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-09 16:57:36 +0530

    \

--- Dependency Injection/Setter Injection (Ref Attribute)/SportsCar.java
+++ Dependency Injection/Setter Injection (Ref Attribute)/SportsCar.java
@@ -1,1 +1,10 @@
+package com.ebaad.SpringSetterInjection;
+
+// This class has been made for showing the example for Reference attribute
+public class SportsCar {
+	public void speed() {
+		System.out.println("Sports cars are faster than the normal ones");
+	}
+
+}
 
