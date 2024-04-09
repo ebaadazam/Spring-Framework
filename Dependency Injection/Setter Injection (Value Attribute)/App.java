7f521cd3a200c740c2a9e7aae814785a6806da46
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-09 16:31:50 +0530

    Update App.java

--- Dependency Injection/Setter Injection (Value Attribute)/App.java
+++ Dependency Injection/Setter Injection (Value Attribute)/App.java
@@ -1,1 +1,17 @@
+package com.ebaad.SpringSetterInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/* Setter injection in Spring Framework is a form of dependency injection where the framework injects
+   dependencies into an object through the object's setter methods. */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Cars obj = (Cars) context.getBean("cars");
+        System.out.println(obj.getMilleage());    
+    }
+}
 
