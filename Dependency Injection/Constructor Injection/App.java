46b864660c2ad68580c0ed8e0ef991279b297a75
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-10 15:21:21 +0530

    Update App.java

--- Dependency Injection/Constructor Injection/App.java
+++ Dependency Injection/Constructor Injection/App.java
@@ -1,1 +1,13 @@
+package com.ebaad.SpringConstructorInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Cars cars = (Cars) context.getBean("cars");
+        System.out.println(cars); // this will call toString() method of Cars class
+    }
+}
 
