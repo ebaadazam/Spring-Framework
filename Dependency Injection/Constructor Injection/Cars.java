46cfa86af53e5c9bc65a49cdd6e75fc2a7ceffdd
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-10 15:21:55 +0530

    Update Cars.java

--- Dependency Injection/Constructor Injection/Cars.java
+++ Dependency Injection/Constructor Injection/Cars.java
@@ -1,1 +1,35 @@
+package com.ebaad.SpringConstructorInjection;
+
+public class Cars {
+	
+	private int milleage;
+	
+	// LuxuryCar Class Object
+	private LuxuryCar luxury;
+	
+	// Constructor
+	public Cars(int milleage, LuxuryCar luxury) {
+		this.milleage = milleage;
+		this.luxury = luxury;
+	}
+	
+	// Setters and getters
+	public void setMilleage(int milleage) {
+		this.milleage=milleage;
+	}
+	public LuxuryCar getLuxury() {
+		return luxury;
+	}
+	public void setLuxury(LuxuryCar luxury) {
+		this.luxury = luxury;
+	}
+	public int getMilleage() {
+		return milleage;
+	}
+
+	@Override
+	public String toString() {
+		return "The milleage of the Car is " + milleage + ". And the luxury car does not care about milleage but it has a safety ratings of about " + luxury.getSafetyrating();
+	}
+}
 
