0b0ffd88ee02b8536dcb9316c9421b4cc47cd74b
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-10 15:22:10 +0530

    Update LuxuryCar.java

--- Dependency Injection/Constructor Injection/LuxuryCar.java
+++ Dependency Injection/Constructor Injection/LuxuryCar.java
@@ -1,1 +1,25 @@
+package com.ebaad.SpringConstructorInjection;
+
+public class LuxuryCar {
+	private float safetyrating; 
+	
+	// Constructor
+	public LuxuryCar(float safetyrating) {
+		this.safetyrating=safetyrating;
+	}
+
+	// Setters and Getters
+	public float getSafetyrating() {
+		return safetyrating;
+	}
+	public void setSafetyrating(float safetyrating) {
+		this.safetyrating = safetyrating;
+	}
+	
+	@Override
+	public String toString() {
+		return "The Luxury Car has a good safety rating of about " + safetyrating;
+	}
+
+}
 
