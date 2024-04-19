2e1932110ad04250cee1ccff0e2b5418e723aef3
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-19 22:53:04 +0530

    Update SpringMvcApplication.java

--- Spring Boot MVC/SpringBootMvcAddTwoNum/SpringMvcApplication.java
+++ Spring Boot MVC/SpringBootMvcAddTwoNum/SpringMvcApplication.java
@@ -1,1 +1,15 @@
+package com.ebaad.SpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringMvcApplication.class, args);
+		System.out.println("Nothing, Just prints on the Console");
+	}
+}
 
