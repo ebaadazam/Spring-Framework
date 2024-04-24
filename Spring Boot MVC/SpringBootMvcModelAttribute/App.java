cbb2622bddf90c8abc17d31e68057dedf8286098
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-04-24 13:02:15 +0530

    Update App.java

--- Spring Boot MVC/SpringBootMvcModelAttribute/App.java
+++ Spring Boot MVC/SpringBootMvcModelAttribute/App.java
@@ -1,1 +1,14 @@
+package com.ebaad.SpringMvcModelAttrDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcModelAttrDemoApplication.class, args);
+		System.out.println("Printing...");
+	}
+}
 
