f2e836fffb9f444ed26bc187ccc6597e3cdf8fc0
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-18 12:57:09 +0530

    Update MyUserDetailsService.java

--- Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/MyUserDetailsService.java
+++ Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/MyUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.telusko.secureapp;
+package com.ebaad.secureapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
