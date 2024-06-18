3496b0726b9ef90c2372890ce9192f44fabc3d21
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-18 12:30:21 +0530

    Add files via upload

---
+++ Spring Security/SecureApp/src/test/java/com/ebaad/SecureApp/SecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebaad.SecureApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
