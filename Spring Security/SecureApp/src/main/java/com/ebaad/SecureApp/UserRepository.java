ed34fa9af8b80fd45c0b91c9e7a30d23dfd679e8
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-18 12:56:29 +0530

    Update UserRepository.java

--- Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/UserRepository.java
+++ Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/UserRepository.java
@@ -1,1 +1,8 @@
+package com.ebaad.secureapp;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUsername(String username);
+
+}
