0afb8f6b552a8d86623cc796561cdc8d6f0e5456
Ebaad Azam <103363195+ebaadazam@users.noreply.github.com>
2024-06-19 17:03:20 +0530

    Update AppSecurityConfig.java

--- Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/AppSecurityConfig.java
+++ Spring Security/SecureApp/src/main/java/com/ebaad/SecureApp/AppSecurityConfig.java
@@ -28,4 +28,21 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter
 		return provider;
 	}
 
+	// for creating our own Login page not the one provided by the Spring Security
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			.csrf().disable()
+			.authorizeRequests().antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.and()
+			.logout().invalidateHttpSession(true)
+			.clearAuthentication(true)
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/logout-success").permitAll()
+	}
+
 }
